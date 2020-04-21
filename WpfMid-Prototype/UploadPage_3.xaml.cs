2b87b85a1efe44aa4d9ced689a72dedf57027d35
christina1024 <christinali1024@gmail.com>
2020-04-21 13:26:16 -0600

    upload page layout fixed

--- WpfMid-Prototype/UploadPage_3.xaml.cs
+++ WpfMid-Prototype/UploadPage_3.xaml.cs
@@ -142,6 +142,11 @@ namespace WpfMid_Prototype
             NavigationService.Navigate(new Uri("/UploadPage_4.xaml", UriKind.RelativeOrAbsolute));
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/UploadPage_1.xaml", UriKind.RelativeOrAbsolute));
+        }
     }
 
     
